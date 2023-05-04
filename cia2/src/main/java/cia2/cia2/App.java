8a886ef063b59f9f0f474ee9666378636075b30e
sudeshv2003 <113581410+sudeshv2003@users.noreply.github.com>
2023-05-04 15:00:07 +0530

    Add files via upload

---
+++ cia2/src/main/java/cia2/cia2/App.java
@@ -0,0 +1,13 @@
+package cia2.cia2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ cia2/src/test/java/cia2/cia2/Task1.java
@@ -0,0 +1,42 @@
+package cia2.cia2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Task1 {
+
+	public static void main(String[] args) {
+		
+		ChromeOptions co=new ChromeOptions();
+		co.addArguments ("--remote-allow-origins=*");
+		WebDriverManager.chromedriver().setup ();
+		WebDriver driver=new ChromeDriver(co);
+		driver.get ("https://www.amazon.in/");
+		driver.manage().window().maximize() ;
+		String currentTitle=driver.getTitle();
+		System.out.println(currentTitle);
+		String expectedTitle="Online Shopping site in India: Shop Online for Mobiles, Books, Watches, Shoes and More - Amazon.in";
+		if(currentTitle.equals(expectedTitle)) {
+			System.out.println("Title Matched");
+		}
+		else {
+			System.out.println("Title not Matched");
+		}
+		driver.findElement(By.xpath("//*[@id=\"nav-link-accountList\"]")).click();
+		WebElement txtBoxl=driver.findElement(By.xpath("//*[@id=\"ap_email\"]"));
+		txtBoxl.sendKeys("8056571552");
+		driver.findElement(By.xpath("//*[@id=\"continue\"]")).click();
+		WebElement txtBox2=driver.findElement(By.xpath("//*[@id=\"ap_password\"]"));
+		txtBox2.sendKeys("Sudesh@2003");
+		driver.findElement(By.xpath("//*[@id=\"signInSubmit\"]")).click();
+		
+		driver.findElement(By.xpath("//*[@id=\"nav-link-accountList\"]")).click();
+		driver.findElement(By.xpath("//*[@id=\"a-page\"]/div[2]/div/div[2]/div[1]/a/div/div")).click();
+		driver.findElement(By.xpath("//*[@id=\"a-autoid-2-announce\"]")).click();
+		driver.findElement(By.xpath("//*[@id=\"ordersInPackage-container\"]/div/div/a[1]")).click();
+	}}
